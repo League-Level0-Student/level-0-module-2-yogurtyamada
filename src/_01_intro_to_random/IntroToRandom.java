771ab60fcbe17ce90c0933c70641604ae84d08f5
League Student <league@iMac-17.attlocal.net>
2019-12-02 19:48:11 -0800

    jhckzcns

--- src/_01_intro_to_random/IntroToRandom.java
+++ src/_01_intro_to_random/IntroToRandom.java
@@ -11,20 +11,22 @@ public class IntroToRandom {
 		int num = 0;
 		
 		//3   Now to make num random. 
+		Random qwe =new Random();
 		//3a. Create an object of the Random class, Hint: Random ran = new Random()
-		
+		num =qwe.nextInt(101);
 		//3b. Next, set the value of num, using .nextInt() to get a random number from the Random object
 		
 
 		//4. Now limit the value of num to be between 0 and 100
 
 		//5. Now limit the value of num to be between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-
+num=qwe.nextInt(51)+25;
 		//6. Challenge: Limit the value of num to be between -222 and 88
-
+num=qwe.nextInt(88)+-222;
 		//1. Print out the value of num
-		
+		System.out.println(num);
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
+		
 	}
 }
--- src/_02_rock_paper_scissors/RockPaperScissors.java
+++ src/_02_rock_paper_scissors/RockPaperScissors.java
@@ -6,6 +6,7 @@ package _02_rock_paper_scissors;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -75,16 +76,16 @@ public class RockPaperScissors extends JPanel implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         
         //1. Run the program and play many rounds of Rock Paper Scissors. Does the computer always choose the same thing?
-
+//yes retard
         //2. Change the value of opponentSelection to be a random number between 0 and 2;
 	//   Don't forget to create a Random object.
-	    
+	    Random qwe=new Random();qwe.nextInt(3);
         //2. Change the value of opponentSelection to be a random number between 0 and 2; 
-        int opponentSelection = 0;
+        int opponentSelection = 3;
         
         //3. Run the program again. Is the result different?
  
-        int selection = 0;
+        int selection = 3;
        
         if(e.getSource() == paperButton){
             selection = 1;
